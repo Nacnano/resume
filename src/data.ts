121b2825dc4e8fdc352380259fe29de63a8adf6b
nac <chotpisit080916c@gmail.com>
2023-01-18 20:57:45 +0700

    feat: add google

--- src/data.ts
+++ src/data.ts
@@ -109,7 +109,7 @@ export const projects = [
   {
     name: "MWIT29 Archive",
     details:
-      "A website for archiving memorable information from my high school by allowing the students to edit their data using NextJS with MongoDB and Cloud Storage (Working in Process)",
+      "A website for archiving memorable information from my high school by allowing the students to edit their data using NextJS with MongoDB and Google Cloud Storage (Working in Process)",
     url: "https://github.com/nacnano/mwit29-archive",
   },
   {
