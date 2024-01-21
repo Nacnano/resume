245a02282ef58b5afe5c722faff9e4e3ed6fc78d
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-21 22:50:40 +0700

    feat: change intro layout

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	export let name: string = 'Chotpisit Adunsehawat';
-	export let nickname: string = 'Nac';
 	export let phone: string = '(+66)96-189-6046';
 	export let email: string = 'chotpisit.adu@gmail.com';
 	export let github: string = 'nacnano';
@@ -9,32 +8,35 @@
 	export let website: string = 'nacnano.dev';
 </script>
 
-<div class="flex flex-wrap flex-col sm:flex-row print:flex-row text-sm sm:text-base">
-	<div class="flex-1 text-left sm:py-4 w-48">
-		<p><a href={`tel:${phone}`}>{phone}</a></p>
-		<p><a href={`mailto:${email}`}>{email}</a></p>
-		<p>{location}</p>
-	</div>
-
+<div class="flex flex-col items-center justify-center print:flex-col text-sm sm:text-base gap-2">
 	<h2
-		class="flex-none order-first sm:order-none print:order-none text-4xl sm:text-2xl md:text-3xl lg:text-6xl text-center p-4 print:pt-0"
+		class="flex-none order-first sm:order-none print:order-none text-4xl sm:text-2xl md:text-3xl lg:text-6xl text-centerprint:pt-0"
 	>
 		{name}
-		<span class="block -mt-1 text-base lg:text-lg">({nickname})</span>
 	</h2>
 
-	<div class="flex-1 text-left sm:text-right print:text-right sm:py-4 w-48 text-sm sm:text-base">
+	<div
+		class="w-full flex flex-wrap justify-center items-center gap-1 text-center print:text-center text-sm"
+	>
+		<p>{location}</p>
+		•
 		<p>
 			<a href={`https://github.com/${github}`} target="_blank" rel="noreferrer"
 				>github.com/{github}</a
 			>
 		</p>
+		•
 		<p>
 			<a href={`https://${website}`} target="_blank" rel="noreferrer">{website}</a>
 		</p>
+		•
 		<p>
 			<a href={`https://linkedin.com/in/${linkedin}`} target="_blank" rel="noreferrer">Linkedin</a>
 		</p>
+		•
+		<p><a href={`tel:${phone}`}>{phone}</a></p>
+		•
+		<p><a href={`mailto:${email}`}>{email}</a></p>
 	</div>
 </div>
 
