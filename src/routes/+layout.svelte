17ba2d600bf85a8db8b28b898714dc37d138e442
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-22 14:47:49 +0700

    feat: add head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>Chotpisit Adunsehawat - Resume</title>
+	<meta name="description" content="Resume of Chotpisit Adunsehawat (Nacnano)" />
+</svelte:head>
+
 <slot />
