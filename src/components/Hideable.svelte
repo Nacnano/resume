2a28e1ef4cac7ed97300b18cc7ff0bfbebbae856
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-28 11:10:14 +0700

    chore: add button for hide to prevent key handler

--- src/components/Hideable.svelte
+++ src/components/Hideable.svelte
@@ -5,11 +5,12 @@
 </script>
 
 <div class="group relative" class:web-only={hide} class:text-gray-300={hide} role="button">
-	<span
+	<button
 		on:click|stopPropagation={toggleHide}
 		class="select-none cursor-pointer"
 		class:cursor-copy={hide}
+		aria-label="Hide Content"
 	>
 		<slot />
-	</span>
+	</button>
 </div>
