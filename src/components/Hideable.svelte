75e50af5a92c02a7585d9c36b3556617571ebb89
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-28 11:25:01 +0700

    fix: change hide to span

--- src/components/Hideable.svelte
+++ src/components/Hideable.svelte
@@ -5,12 +5,12 @@
 </script>
 
 <div class="group relative" class:web-only={hide} class:text-gray-300={hide} role="button">
-	<button
+	<span
 		on:click|stopPropagation={toggleHide}
 		class="select-none cursor-pointer"
 		class:cursor-copy={hide}
 		aria-label="Hide Content"
 	>
 		<slot />
-	</button>
+	</span>
 </div>
