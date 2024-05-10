fe07fbece8858a5d47c6673967bedbb2637bbb27
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-05-10 18:31:03 +0700

    docs: add ta for com eng ess

--- src/data/educations.ts
+++ src/data/educations.ts
@@ -8,7 +8,8 @@ export const educations: IEducation[] = [
 		details: [
 			'Achieved 4.00 GPA for 3 semesters',
 			// 'Achieved 4.00 GPA for 8 semesters',
-			'Participated in an exchange program with Guangxi University, China',
+			// 'Participated in an exchange program with Guangxi University, China',
+			'Teaching Assistant: Computer Engineering Essential (Backend)',
 			'Related Coursework: Data Structures, Algorithm Design, Programming Methodology, Pattern Recognition'
 		],
 		hide: false
