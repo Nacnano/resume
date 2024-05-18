23280fe05570ea67a8f20c36bdc8b8594f54332a
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-05-18 10:55:58 +0700

    docs: add others

--- src/data/others.ts
+++ src/data/others.ts
@@ -6,11 +6,10 @@ export const others: IOther[] = [
 		detail:
 			'Extra curricular activities: President of School International Chess Club, Head of Information Department of High School Yearbook Committee'
 	},
-	{ detail: 'Areas : Coding, Sports, Piano', hide: true },
-	{ detail: 'Games : Music games, Puzzle', hide: true },
 	{
 		detail:
-			'Non-tech-related working experiences/achievements: Academic Tutor (Math/Physics/Chemistry/Bio/English), Qualified for the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science, Got accepted to Faculty of Medicine, Chulalongkorn University',
-		hide: true
-	}
+			'Non-tech experiences/achievements: Academic Tutor (Math/Physics/Chemistry/Biology/English), Qualified for the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science worldwide, Got accepted to Faculty of Medicine, Chulalongkorn University'
+	},
+	{ detail: 'Areas : Badmianton, Football, Table Tennis, Piano', hide: true },
+	{ detail: 'Games : Music games, Puzzle, Strategy games', hide: true }
 ];
