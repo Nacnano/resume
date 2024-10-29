ae8f5ce73775131c0d3adb7d01a379d7e64c200e
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-10-29 10:56:27 +0700

    remove non-tech

--- src/data/others.ts
+++ src/data/others.ts
@@ -7,11 +7,13 @@ export const others: IOther[] = [
 	},
 	{
 		detail:
-			'Extra curricular activities: President of School International Chess Club, Head of Information Department of High School Yearbook Committee'
+			'Extra curricular activities: President of School International Chess Club, Head of Information Department of High School Yearbook Committee',
+		hide: true
 	},
 	{
 		detail:
-			'Non-tech experiences/achievements: Academic Tutor (Math/Physics/Chemistry/Biology/English), Qualified for the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science worldwide, Got accepted to Faculty of Medicine, Chulalongkorn University'
+			'Non-tech experiences/achievements: Academic Tutor (Math/Physics/Chemistry/Biology/English), Qualified for the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science worldwide, Got accepted to Faculty of Medicine, Chulalongkorn University',
+		hide: true
 	},
 	{ detail: 'Areas : Badmianton, Football, Table Tennis, Piano', hide: true },
 	{ detail: 'Games : Music games, Puzzle, Strategy games', hide: true }
