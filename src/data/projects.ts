1f3c805e84961186e6cd4ec7b06b518969cd6ff4
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-11-10 15:38:06 +0700

    add data science project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,24 @@ import type { IProject } from '../types';
 
 export const projects: IProject[] = [
 	{
+		name: 'Thai Context Understanding of LLMs',
+		details:
+			'To benchmark Thai context understanding in LLMs, use Visual Genome data to generate Thai-language questions based on object attributes and relationships for evaluating model performance.',
+		url: 'https://github.com/Nacnano/indiv',
+	},
+	{
+		name: 'Stock Return Prediction',
+		details:
+			'Developed machine learning models to predict stock returns using historical S&P 500 data, achieving 42.52% annual returns with LSTM from back-tesing with risk management. Implemented feature importance for multiple models',
+		url: 'https://github.com/Nacnano/stock-machine-learning-project/',
+	},
+	{
+		name: 'Multimedia Learning Efficiency Prediction in Student using MASK R-CNN',
+		details:
+			"Usage of MASK R-CNN model for Image Segmentation and Random Forest model for Factor Identification to analyse multimedia effects on students' learning efficiency",
+		url: 'https://github.com/Nacnano/predicting-and-comparing-learners-interest-in-note-taking-from-multimedia-using-a-machine-learning-',
+	},
+	{
 		name: 'CU Get Reg',
 		details:
 			"Thinc's open source project to provide courses data to Chulalongkorn students and plan their enrollments. Contributed by developing its Backoffice backend service using NestJS and GraphQL for managing over 700 reviews and 500 elective courses with over 15,000 maximum users per week",
@@ -16,7 +34,7 @@ export const projects: IProject[] = [
 	{
 		name: 'CU First Date and Rub Puen Kao Mai 2024',
 		details:
-			'A website developed for CU First Date and Rub Puen Kao Mai activities at Chulalongkorn University, featuring functionalities such as House Selection, Walk Rally QR code scanning, etc. Developed the registration and profile editing pages, as well as key components for this large-scale event management platform',
+			'A website developed for CU First Date and Rub Puen Kao Mai activities at Chulalongkorn University, featuring functionalities such as House Selection, Walk Rally QR code scanning, etc. Developed the registration and profile editing pages, components for this event management platform',
 		url: 'https://github.com/isd-sgcu/firstdate-rpkm67-frontend/'
 	},
 	{
@@ -30,27 +48,6 @@ export const projects: IProject[] = [
 		details:
 			"Thinc and Cleverse's 5 days Hack to School project for creating a website by given technical requirements. Developed its backend using Nestjs and Prisma, and deploying the project using Docker and Google Cloud Run",
 		url: 'https://github.com/thinc-org/hacktoschool-fryingchicken',
-		hide: true
-	},
-	{
-		name: 'Thai Context Understanding of LLMs',
-		details:
-			'To benchmark Thai context understanding in LLMs, use Visual Genome data to generate Thai-language questions based on object attributes and relationships for evaluating model performance.',
-		url: 'https://github.com/Nacnano/indiv',
-		hide: true
-	},
-	{
-		name: 'Stock Return Prediction',
-		details:
-			'Developed machine learning models to predict stock returns using historical S&P 500 data, achieving 42.52% annual returns with LSTM from back-tesing with risk management. Implemented feature importance for multiple models',
-		url: 'https://github.com/Nacnano/stock-machine-learning-project/',
-		hide: true
-	},
-	{
-		name: 'School Computer Science Project',
-		details:
-			"Usage of MASK R-CNN model for Image Segmentation and Random Forest model for Factor Identification to analyse multimedia effects on students' learning efficiency",
-		url: 'https://github.com/Nacnano/predicting-and-comparing-learners-interest-in-note-taking-from-multimedia-using-a-machine-learning-',
 		hide: true
 	},
 	{
