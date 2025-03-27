806fdd8872c545909d8b798dcb2683795cfbbfb0
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-03-27 09:52:28 +0700

    add legaltech

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -2,6 +2,11 @@ import type { IAchievement } from '../types';
 
 export const achievements: IAchievement[] = [
 	{
+		name: 'Chula LegalTech Year 6 (2025)',
+		details:
+			'Won the first place and 50,000 bath prize for designing and developing a web application for facilitating job matching for youth. My role was to developed its frontend and OCR backend service using NextJS and FlaskAPI'
+	},
+	{
 		name: "Thinc's 10 Days in 10 Years Project (2022)",
 		details:
 			"Won the first place from over 50 teams in a competition for implementing a web application for solving specific problems in 10 days for faciliating student's withdrawal. My role was to designed and developed its backend service and database"
