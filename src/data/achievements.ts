05963dcf8974d487bcb776406cab839a4eef7470
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-10-17 22:06:53 +0700

    docs: fix grammar

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -9,7 +9,7 @@ export const achievements: IAchievement[] = [
 	{
 		name: 'JUMP Thailand Hackathon by AIS Academy (2025)',
 		details:
-			'Achieved second runner-up among more than 700 teams across Thailand for developing an AI-assisted massage teaching system for blind people using head-mounted cameras and AI instructors. My role was to develop and deploy the instructor model using multimodal language models and computer vision techniques to detect muscles and postures during massage sessions and provide real-time feedback to students.'
+			'Achieved second runner-up and a 40,000 Baht prize among more than 700 teams across Thailand for developing an AI-assisted massage teaching system for blind people using head-mounted cameras and AI instructors. My role was to develop and deploy the instructor model using multimodal language models and computer vision techniques to detect muscles and postures during massage sessions and provide real-time feedback to students.'
 	},
 	{
 		name: 'AI Hackathon Powered by MITR PHOL (2025)',
@@ -19,7 +19,7 @@ export const achievements: IAchievement[] = [
 	{
 		name: "Thinc's 10 Days in 10 Years Project (2022)",
 		details:
-			'Won first place among over 50 teams in a 10-day competition to develop a web application solving real-world problems — in this case, facilitating student withdrawals. My role was to design and develop the backend service and database.'
+			'Won first place among more than 50 teams in a 10-day competition to develop a web application solving real-world problems — in this case, facilitating student withdrawals. My role was to design and develop the backend service and database.'
 	},
 	{
 		name: 'Computer Olympiad Camp (2019–2022)',
