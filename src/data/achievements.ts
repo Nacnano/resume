a80422280e9f3bd0b3cb48de178f92763f6f44f6
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-10-17 21:53:01 +0700

    docs: add hackathons

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -7,6 +7,16 @@ export const achievements: IAchievement[] = [
 			'Won the first place and 50,000 bath prize for designing and developing a web application for facilitating job matching for youth. My role was to developed its frontend and OCR backend service using NextJS and FlaskAPI'
 	},
 	{
+		name: 'JUMP Thailand Hackathon by AIS Academy (2025)',
+		details:
+		'Got the second runner-up from more than 700 teams from all over Thailand for facilitating massage teaching for blind people using head cameras and AI teachers. My role was to developed and deployed the teacher model using multimodal language model and computer vision techniques for detecting muscles and postures during massage and giving real-time feedbacks to students'
+	},
+	{
+		name: 'AI Hackathon Powered by MITR PHOL (2025)',
+		details:
+		'Got the second runner-up from 156 teams for machinery breakdown prevention in sugar harvesting machines. My role was to developed the forecasting model from several sensory data using various machine learning models and techniques'
+	},
+	{
 		name: "Thinc's 10 Days in 10 Years Project (2022)",
 		details:
 			"Won the first place from over 50 teams in a competition for implementing a web application for solving specific problems in 10 days for faciliating student's withdrawal. My role was to designed and developed its backend service and database"
