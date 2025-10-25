305082d834531dc94aa1a2fe023ac69307ff5296
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-10-25 16:12:02 +0700

    add opengraph

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,15 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://resume.nacnano.dev" />
 	<meta property="og:site_name" content="Chotpisit Adunsehawat Resume" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Chotpisit Adunsehawat - Resume" />
+	<meta name="twitter:description" content="Resume of Chotpisit Adunsehawat (Nacnano)" />
+	<meta name="twitter:image" content="/sample-resume.png" />
+	<meta name="twitter:creator" content="@Nacnano1" />
+
+	<meta property="og:image:alt" content="Preview image of Chotpisit Adunsehawat's resume" />
+	<meta property="og:locale" content="en_US" />
 </svelte:head>
 
 <slot />
