f5562ab6c3eb5f9f1afeaef611c3aaa70eee0f0b
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-10-25 16:02:58 +0700

    add opengraph

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,13 @@
 <svelte:head>
 	<title>Chotpisit Adunsehawat - Resume</title>
 	<meta name="description" content="Resume of Chotpisit Adunsehawat (Nacnano)" />
+
+	<meta property="og:title" content="Chotpisit Adunsehawat - Resume" />
+	<meta property="og:description" content="Resume of Chotpisit Adunsehawat (Nacnano)" />
+	<meta property="og:image" content="%sveltekit.assets%/sample-resume.png" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://resume.nacnano.dev" />
+	<meta property="og:site_name" content="Chotpisit Adunsehawat Resume" />
 </svelte:head>
 
 <slot />
