489e1099f76a7d2b8e32bfb8b8185a25081f740e
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-12-03 17:33:06 +0700

    fix others

--- src/data/others.ts
+++ src/data/others.ts
@@ -6,19 +6,25 @@ export const others: IOther[] = [
 			'Languages: Thai (Native), English (Working Proficient / IELTS 7.5), Chinese (Elementary)'
 	},
 	{
-		detail: "Accepted into the SCB Data Scientist Internship Program 2025 (Declined the offer)",
+		detail: 'Accepted to the SCB Data Scientist Internship Program 2025 (Declined the offer)',
+		hide: true
+	},
+	{
+		detail: 'Accepted to the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science worldwide (declined the offer)',
+		hide: true
+	},
+	{
+		detail: 'Accepted to Faculty of Medicine, Chulalongkorn University (Declined the offer)',
+		hide: true
+	},
+	{
+		detail: 'Part-time Academic Tutor (Math/Computer/Physics/Chemistry/Biology/English)',
 		hide: true
 	},
 	{
 		detail:
-			'Extra curricular activities: President of School International Chess Club, Head of Information Department of High School Yearbook Committee',
+			'Extra-curricular activities: President of School International Chess Club, Head of Information Department of High School Yearbook Committee',
 		hide: true
 	},
-	{
-		detail:
-			'Non-tech experiences/achievements: Academic Tutor (Math/Physics/Chemistry/Biology/English), Qualified for the Thai Scholarship (ทุนกระทรวงวิทยฯ) for studying Computer Science worldwide, Got accepted to Faculty of Medicine, Chulalongkorn University',
-		hide: true
-	},
-	{ detail: 'Areas : Badminton, Football, Table Tennis, Running, Piano', hide: true },
-	{ detail: 'Games : Music games, Puzzle, Strategy games', hide: true },
+	{ detail: 'Hobbies: Badminton, Football, Table Tennis, Running, Games, Piano, Music Jamming', hide: true }
 ];
