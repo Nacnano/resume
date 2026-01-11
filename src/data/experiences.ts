cc2d226ce32006972268f0f4f8706811823ba3f7
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2026-01-12 00:46:04 +0700

    fix date

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -14,7 +14,7 @@ export const experiences: IExperience[] = [
 		company: 'SCBX',
 		location: 'Bangkok, Thailand',
 		url: 'https://scbx.com',
-		years: ['August, 2025', 'December, 2025'],
+		years: ['August', 'December, 2025'],
 		details: [
 			'Conducted quantitative finance and AI research on U.S. and Thai stock markets using transfer learning and ESG metrics in cooperation with Stanford University',
 			'Assisted in testing an internal chatbot for AI technology and cybersecurity risk management'
